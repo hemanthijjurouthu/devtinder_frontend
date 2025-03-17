abf57fd7dc41de9b64d7f71acbc5b6d7a2fefacf
IJJUROUTHU HEMANTH <hemanthijjurouthu@gmail.com>
2025-03-17 10:48:43 +0530

    added

--- devtinder/utils/socket.js
+++ devtinder/utils/socket.js
@@ -1,10 +1,17 @@
-import io from 'socket.io-client';
-import { BASE_URL } from './Constants';
+import io from "socket.io-client";
+import { BASE_URL } from "./Constants";
 
 export const createSocketConnection = () => {
-    if (location.hostname === 'localhost') {
-        return io(BASE_URL);
+    if (location.hostname === "localhost") {
+        return io(BASE_URL, {
+            transports: ["websocket", "polling"], 
+            withCredentials: true
+        });
     } else {
-        return io("/", { path: "/api/socket-io" });
+        return io("https://ijjurouthu.duckdns.org", {
+            path: "/api/socket-io/", // Ensure this matches Nginx location
+            transports: ["websocket", "polling"],
+            withCredentials: true
+        });
     }
 };
